4b6f1064d534a460cc1d29288d6179f58c203e9f
AthanThom <konialis789@gmail.com>
2023-08-11 10:22:29 +0300

    api exception update

---
+++ src/api.exception.ts
@@ -0,0 +1,12 @@
+import { HttpException } from "@nestjs/common";
+export class ApiException extends HttpException {
+  constructor(message: string, id: string, statusCode: number) {
+    super(
+      JSON.stringify({
+        id: id,
+        message: message,
+      }),
+      statusCode
+    );
+  }
+}
